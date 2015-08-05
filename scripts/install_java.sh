de3c07c6030bf773b994edfeada29c792a05c1cc
Cai Cooper <caicooper82@gmail.com>
2015-08-05 17:21:00 +0100

    Refactored java installation script

--- scripts/install_java.sh
+++ scripts/install_java.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 echo "Installing Java..."
-cd /opt/
-wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u40-b25/jdk-8u40-linux-x64.tar.gz"
-tar xzf jdk-8u40-linux-x64.tar.gz
-ln -s /opt/jdk1.8.0_40 /opt/java8
+mkdir -p /opt/java/1.8.0_40/
+wget --quiet --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u40-b25/jdk-8u40-linux-x64.tar.gz" -O /opt/java/1.8.0_40/jdk-8u40-linux-x64.tar.gz
+tar xzf /opt/java/1.8.0_40/jdk-8u40-linux-x64.tar.gz
 
-sudo echo "JAVA_HOME=/opt/jdk1.8.0_40/" >> /etc/environment
+sudo echo "JAVA_HOME=/opt/java/1.8.0_40/" >> /etc/environment
 # add to PATH
-escaped_java_home=$(echo "/opt/jdk1.8.0_40/" | sed 's/\//\\\//g')
+escaped_java_home=$(echo "/opt/java/1.8.0_40/" | sed 's/\//\\\//g')
 sed -i "s/\(^PATH.*\)\"$/\1:${escaped_java_home}\/bin\"/" /etc/environment
+# clear up source
+rm /opt/java/1.8.0_40/jdk-8u40-linux-x64.tar.gz
