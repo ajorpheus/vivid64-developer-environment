1b1d88c65572e76b2018c7ff635c01bf011a2fbb
Cai Cooper <Cai.Cooper@monitise.com>
2015-08-07 11:44:45 +0100

    Minor fix to intellij installation script

--- scripts/install_intellij.sh
+++ scripts/install_intellij.sh
@@ -6,10 +6,9 @@ echo "JDK_HOME=${JDK_HOME}" >> /etc/environment
 intellij="http://download.jetbrains.com/idea/ideaIC-14.1.4.tar.gz"
 product="intellij"
 version=$(echo ${intellij} | sed 's/^.*idea\/\([a-zA-Z\-]*.[a-z]*\)-\([0-9\-\.]*\).tar.gz/\2/')
-mkdir -p /opt/${product}/${version}
+mkdir -p /opt/${product}/${version}/
 chmod 777 -R /opt/${product}/
-wget --quiet -nv ${intellij} -O /opt/${product}/${version}
-tar zxf `basename ${intellij}` -C /opt/${product}/${version} --strip 1 >/dev/null 2>/dev/null
-#product_executable="$(find /opt/${product}/${version}/bin -type f -name \"idea*.sh\" 2>/dev/null | head -1)"
+wget --quiet -nv ${intellij} -O /opt/${product}/${version}/$(basename ${intellij})
+tar zxf /opt/${product}/${version}/$(basename ${intellij}) -C /opt/${product}/${version}/ --strip 1 >/dev/null 2>/dev/null
 ln -sf /opt/${product}/${version}/bin/idea.sh /usr/bin/${product}
-rm $(basename ${intellij})
+rm /opt/${product}/${version}/$(basename ${intellij})
