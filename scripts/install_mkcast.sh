b73d6242e77b47e83350cf0ec4203f81b017994b
Cai Cooper <Cai.Cooper@monitise.com>
2015-08-07 13:10:31 +0100

    Minor fix to mkcast installation script

--- scripts/install_mkcast.sh
+++ scripts/install_mkcast.sh
@@ -3,11 +3,11 @@
 echo "Installing mkcast..."
 mkdir -p /opt/mkcast/
 mkcast="https://github.com/KeyboardFire/mkcast/archive/master.zip"
-wget --quiet -nv ${product_archive} -O /opt/mkcast/
-unzip /opt/mkcast/`basename ${product_archive}` >/dev/null 2>/dev/null
+wget --quiet -nv ${mkcast} -O /opt/mkcast/$(basename ${mkcast})
+unzip /opt/mkcast/$(basename ${mkcast}) >/dev/null 2>/dev/null
 chown -R vagrant:vagrant /opt/mkcast/mkcast-master
 ln -s /opt/mkcast/mkcast-master/mkcast /usr/local/bin/mkcast
 ln -s /opt/mkcast/mkcast-master/newcast /usr/local/bin/newcast
 apt-get -y -qq install wmctrl >/dev/null 2>/dev/null
 apt-get -y -qq install byzanz >/dev/null 2>/dev/null
-rm /opt/mkcast/ `basename ${product_archive}`
+rm /opt/mkcast/$(basename ${mkcast})
